43c5a8d4530687bab5d5e07224a34b888a0a502a
NdivhoNemz <ndivhonematandani@gmail.com>
2023-10-18 04:00:00 -0700

    update main.h

--- main.h
+++ main.h
@@ -10,16 +10,4 @@ int _strlen(char *string);
 int character_count(void);
 extern int num_of_characters; /* external variable */
 
-/* FLAGS */
-#define F_MINUS 1
-#define F_PLUS 2
-#define F_ZERO 4
-#define F_HASH 8
-#define F_SPACE 16
-
-/* Funciotns to handle other specifiers */
-int get_flags(const char *format, int *i);
-int get_width(const char *format, int *i, va_list list);
-
-
 #endif /* MAIN_H */
